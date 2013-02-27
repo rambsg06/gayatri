28a579b70cad81ea88f8b0e12fa24b37c7a70532
Rameswar Rath <ram@gmicro.us>
2013-02-27 09:36:51 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gayatri::Application.config.secret_token = 'e5a8ed5ec43636f5659491565ace13edcf2ab6dfee5567140455be8f04af25ae66fa96b7c7ce27c57fe3b8712c045ba188cffca5f7af988c45ff2e90a25f2c52'
